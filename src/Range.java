268e961bcfa63d18080b1d45b96ba2ced1ef475d
Rescue9 <Rescue9@yahoo.com>
2012-08-13 21:51:18 -0500

    upload Lab1 & zip additional .gitignore initial upload

---
+++ src/Range.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Range {
+
+	public static void main(String[] args) {
+		System.out.println("Greetings.");
+		int min, max;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Enter a minimum and maximum value, leaving a space getweent the two numbers");
+		
+		min = keyboard.nextInt();
+		max = keyboard.nextInt();
+		
+		System.out.println("The number of values in the range from " + min + " to " + max + " is ");
+		System.out.println(max-min+1);
+
+	}
+
+}
