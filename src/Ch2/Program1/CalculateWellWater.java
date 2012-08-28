a2c9a99f868c2ffceaefbbc584a94854e58a9eeb
Rescue9 <Rescue9@yahoo.com>
2012-08-27 20:53:00 -0500

    started Ch2: Program1; CalculateWellWater.java

---
+++ src/Ch2/Program1/CalculateWellWater.java
@@ -0,0 +1,42 @@
+package Ch2.Program1;
+
+/*
+ * Program: CalculateWellWater.java
+ * Class: CIS 149
+ * Date: August 27, 2012
+ * Programmer Andrew Buskov
+ * Purpose: To calculate the amount of usable water in a well with given dimensions.
+ * 
+ * Many private water wells produce only 1 or 2 gallons of water per minute.
+One way to avoid running out of water with these low-yield wells is to use a
+holding tank. A family of 4 will use about 250 gallons of water per day. How-
+ever, there is a “natural” water holding tank in the casing (i.e., the hole) of the
+well itself. The deeper the well, the more water that will be stored that can be
+pumped out for household use. But how much water will be available?
+
+Write a program that allows the user to input the radius of the well
+casing in inches (a typical well will have a 3-inch radius) and the depth
+of the well in feet (assume water will fill this entire depth, although in
+practice that will not be true since the static water level will generally be
+50 feet or more below the ground surface). The program should output
+the number of gallons stored in the well casing. For your reference:
+The volume of a cylinder is PIr2h , where r is the radius and h is the height.
+1 cubic foot = 7.48 gallons of water.
+
+For example, a 300-foot well full of water with a radius of 3 inches for the
+casing holds about 441 gallons of water—plenty for a family of 4 and no
+need to install a separate holding tank.
+
+ */
+
+public class CalculateWellWater {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
