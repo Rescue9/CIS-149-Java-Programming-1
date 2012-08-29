ca6800afa4c8fbf9f35bb8d66c291e3e166ebf9c
Rescue9 <Rescue9@yahoo.com>
2012-08-28 19:18:38 -0500

    finished Ch2 Program1: CalculateWellWater

--- src/Ch2/Program1/CalculateWellWater.java
+++ src/Ch2/Program1/CalculateWellWater.java
@@ -7,40 +7,26 @@ package Ch2.Program1;
  * Programmer Andrew Buskov
  * Purpose: To calculate the amount of usable water in a well with given dimensions.
  * 
- * Many private water wells produce only 1 or 2 gallons of water per minute.
-One way to avoid running out of water with these low-yield wells is to use a
-holding tank. A family of 4 will use about 250 gallons of water per day. How-
-ever, there is a “natural” water holding tank in the casing (i.e., the hole) of the
-well itself. The deeper the well, the more water that will be stored that can be
-pumped out for household use. But how much water will be available?
-
-Write a program that allows the user to input the radius of the well
-casing in inches (a typical well will have a 3-inch radius) and the depth
-of the well in feet (assume water will fill this entire depth, although in
-practice that will not be true since the static water level will generally be
-50 feet or more below the ground surface). The program should output
-the number of gallons stored in the well casing. For your reference:
-The volume of a cylinder is PIr2h , where r is the radius and h is the height.
-1 cubic foot = 7.48 gallons of water.
-
-For example, a 300-foot well full of water with a radius of 3 inches for the
-casing holds about 441 gallons of water—plenty for a family of 4 and no
-need to install a separate holding tank.
-
+ * Added method to round numbers to a more usable format.
  */
  
-import java.util.*
+import java.text.DecimalFormat;
+import java.util.*;
 
 public class CalculateWellWater {
  
- public static final double PI = 3.14159
- public static final/double CU_FT_H2O = 7.48
+ public static final double PI = 3.14159;
+ public static final double CU_FT_H2O = 7.48;
 
 	public static void main(String[] args) {
 		
 		// let's declare our variables first
 		double radius;
 		double height;
+		double heightInFeet;
+		double totalCubicFt;
+		double totalGalonage;
+		
 		Scanner keyboard;
 		
 		// get our variables from the user
@@ -50,11 +36,22 @@ public class CalculateWellWater {
 		
 		System.out.println("Please enter the height of the well: ");
 		height = keyboard.nextDouble();
+		heightInFeet = height / 144;
 		
-		// perform our calculations and.respond to the user
+		// perform our calculations and respond to the user
+		totalCubicFt = PI * (radius * radius) * heightInFeet;
+		System.out.println("The total Cu. Ft. of the well is: " + roundMyAnswer(totalCubicFt));
 		
+		totalGalonage = totalCubicFt * CU_FT_H2O;
+		System.out.println("The total number of gallons in the well is: " + roundMyAnswer(totalGalonage));
 		
-
+	}
+	
+	// lets round this stuff..... it's nasty as a double!
+	
+	public static Double roundMyAnswer(double num) {
+		DecimalFormat twoPlaces = new DecimalFormat("#.##");
+		return Double.valueOf(twoPlaces.format(num));
 	}
 
 }
