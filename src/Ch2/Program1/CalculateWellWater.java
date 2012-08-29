2df3e1cb00642fd047d8b177ff8d15f085e222e3
Rescue9 <Rescue9@yahoo.com>
2012-08-29 14:19:01 -0500

    changed program for correctness in the assignment. I should have used the formula as such:
    variableName = (int) ((Math.PI * (variableForRadius/ 12.0)
    *(variableForRadius / 12.0)* varibleForDepth) * variableforCubicFeet);

--- src/Ch2/Program1/CalculateWellWater.java
+++ src/Ch2/Program1/CalculateWellWater.java
@@ -15,7 +15,7 @@ import java.util.*;
 
 public class CalculateWellWater {
  
- public static final double PI = 3.14159;
+ //public static final double PI = 3.14159; // original program, edited for correctness
  public static final double CU_FT_H2O = 7.48;
 
 	public static void main(String[] args) {
@@ -23,8 +23,8 @@ public class CalculateWellWater {
 		// let's declare our variables first
 		double radius;
 		double height;
-		double heightInFeet;
-		double totalCubicFt;
+		//double heightInFeet; // original program, edited for correctness
+		//double totalCubicFt; // original program, edited for correctness
 		double totalGalonage;
 		
 		Scanner keyboard;
@@ -36,22 +36,28 @@ public class CalculateWellWater {
 		
 		System.out.println("Please enter the height of the well: ");
 		height = keyboard.nextDouble();
-		heightInFeet = height / 144;
+		//heightInFeet = height / 144; // original program, edited for correctness
 		
 		// perform our calculations and respond to the user
+		/*	removed for correctness in actual programming assignment
 		totalCubicFt = PI * (radius * radius) * heightInFeet;
 		System.out.println("The total Cu. Ft. of the well is: " + roundMyAnswer(totalCubicFt));
 		
 		totalGalonage = totalCubicFt * CU_FT_H2O;
 		System.out.println("The total number of gallons in the well is: " + roundMyAnswer(totalGalonage));
+		*/
+		totalGalonage = (int) ((Math.PI * (radius / 12.0) * (radius / 12.0)	* height) * CU_FT_H2O);
+		System.out.println("The well contains " + totalGalonage + " gallons.");
+
 		
 	}
 	
+	/* removed for correctness in actual programming assignment
 	// lets round this stuff..... it's nasty as a double!
 	
 	public static Double roundMyAnswer(double num) {
 		DecimalFormat twoPlaces = new DecimalFormat("#.##");
 		return Double.valueOf(twoPlaces.format(num));
-	}
+	}*/
 
 }
