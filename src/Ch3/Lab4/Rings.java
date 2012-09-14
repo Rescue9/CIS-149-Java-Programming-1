1879c89fadbace1b7e3d3be902ce49dcffd1cbe9
Rescue9 <Rescue9@yahoo.com>
2012-09-14 16:08:30 -0500

    removed Ch3Lab docs removed tmp folder for Rings (shouldn't have been committed anyway... must have happened in AIDE) added Ch3Program docs added Ch3Lab4 zip

--- src/Ch3/Lab4/Rings.java
+++ src/Ch3/Lab4/Rings.java
@@ -13,9 +13,17 @@ import java.awt.*;
 
 public class Rings extends JApplet{
 	
+	// added to supress warning:
+	// the serializable class icon does not declare a static final serialVersionUID field of type long
+	private static final long serialVersionUID = 5302949132430816477L;
+
 	public static final int RING_DIAMETER = 40;
 	public static final int RING_X_OFFSET = 25;
 	public static final int RING_Y_OFFSET = 25;
+	
+	public static void main(String[] args){
+		
+	}
 	
 	public void paint(Graphics canvas){
 		
