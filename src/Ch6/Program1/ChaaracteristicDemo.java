933931c0d0b9537d1c024c2c76e75c3449367306
Rescue9 <Rescue9@yahoo.com>
2012-10-31 09:59:09 -0500

    initial Ch6P1 code. getters & setters work.

---
+++ src/Ch6/Program1/ChaaracteristicDemo.java
@@ -0,0 +1,31 @@
+/*
+ * Program: Characteristic Demo
+ * Programmer: Andrew Buskov
+ * Class: CIS 149
+ * Date: Oct 31, 2012
+ * Purpose: To demonstrate the characteristic class.
+*/
+package Ch6.Program1;
+
+
+import java.util.Scanner;
+
+public class ChaaracteristicDemo {
+	
+	static Characteristic myTraits = new Characteristic();
+	Scanner keyboard = new Scanner(System.in);
+	
+	public static void main(String[] args){
+		
+		for (int i = 1; i <= 2; i++){
+			
+			System.out.println("What is your rating for " + myTraits.getDescription(i));
+			int myInput = myTraits.setInput();
+			myTraits.isValid(myInput);
+			myTraits.setRating(i, myInput);
+			System.out.println();
+			int returnInfo = myTraits.getRating(i);
+			System.out.println("Displaying the rating we got: " + myTraits.getDescription(i) + " is rated as " + myTraits.getRating(i));
+		}
+	}
+}
---
+++ src/Ch6/Program1/Characteristic.java
@@ -0,0 +1,83 @@
+/*
+ * Program: Characteristic.java
+ * Programmer: Andrew Buskov
+ * Class: CIS 149
+ * Date: Oct 31, 2012
+ * Purpose: To determine how compatable two people are.
+*/
+
+package Ch6.Program1;
+
+import java.util.Scanner;
+
+public class Characteristic {
+	
+	private int input = -1;
+	private String likesSports = "likes sports";
+	private int sportsRating = 0;
+	private String likesBeaches = "likes beaches";
+	private int beachesRating = 0;
+	
+	public int isValid(int rating){
+		
+		if (rating < 1 | rating > 10){
+			
+			System.out.println("Your input is not valid.");
+			makeValidInput();
+		} 
+		return rating;
+		
+	}
+	
+	public String getDescription(int question){
+		
+		switch (question){
+		
+		case 1: return likesSports;
+		case 2: return likesBeaches;	
+		default: return null;
+		}
+	}
+	
+	public int getRating(int question){
+		
+		switch (question){
+		
+		case 1: return sportsRating;
+		case 2: return beachesRating;
+		default: return 0;
+		}
+	}
+	
+	private void getCompatability(){
+		
+		
+	}		
+	
+	public void setRating(int question, int rating){
+		
+		switch (question){
+		
+		case 1: sportsRating = rating;
+				break;
+		case 2: beachesRating = rating;
+				break;			
+		}
+	}
+	
+	public int setInput(){
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter an integer from 1 to 10");
+		input = keyboard.nextInt();
+		return input;
+	}
+	
+	private void makeValidInput(){
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter an integer from 1 to 10");
+		input = keyboard.nextInt();
+		isValid(input);
+	}
+}
