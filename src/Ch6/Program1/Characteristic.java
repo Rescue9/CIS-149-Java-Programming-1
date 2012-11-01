2aba1cfccabeb87c58b3c9457a08c7b644e4e738
Rescue9 <Rescue9@yahoo.com>
2012-11-01 10:32:12 -0500

    altered the characteristic class completely to match the description given by the instructor. I still don't understand the problem though... hard to find a solution when you don't understand the problem.

--- src/Ch6/Program1/Characteristic.java
+++ src/Ch6/Program1/Characteristic.java
@@ -16,48 +16,69 @@ public class Characteristic {
 	private int rating;
 	private int input;
 	
-	public Characteristic(){
-		setDescription("No Description");
-		setaRating(0);
-	}
-	
 	public Characteristic(String interest){
 		setDescription(interest);
 		rating = 0;
 	}
 	
-	public Characteristic(String interest, int input){
-		setDescription(interest);
-		setaRating(input);
-	}
-	
-	private void setDescription(String interest) {
-		description = interest;
-	}
-
-	public int setRating(){
-		
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Please enter an integer from 1 to 10");
-		input = keyboard.nextInt();
-		// System.out.println(input); // used for testing
-		return input;
+	public boolean isValid(int input){
+		if (input >= 1 && input <= 10){
+			setaRating(input);
+			return true;
+		} else {
+		return false;}
 	}
 	
 	public int setaRating(int input){
 		rating = input;
 		return rating;
 	}
-	
-	public boolean isValid(int rating){
-		
-		if (rating >= 1 && rating <= 10){
-			setaRating(rating);
-			return true;
-		} else {
-		return false;}
+
+	public void setDescription(String interest) {
+		description = interest;
+	}
+
+	public int setRating(){
+		System.out.println("What is your rating for likes " + description + "?");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter an integer from 1 to 10");
+		input = keyboard.nextInt();
+		// System.out.println(input); // used for testing
+		if (!this.isValid(input)){
+			this.setRating();
+		} 
+		return input;
 	}
 	
+	public void toAString(){
+		System.out.println("Displaying the rating we got: likes " + description + " is rated as " + rating);
+	}
+	
+	public double getCompatability(Characteristic otherRating){
+		if (!isMatch(otherRating)){
+			return 0.0;
+		} else {
+		return getCompatabilityMeasure(otherRating);
+		}
+	}
+
+	public boolean isMatch(Characteristic otherRating){
+		System.out.println(description.equals(otherRating.getDescription()));
+		return description.equals(otherRating.getDescription());
+		
+	}
+	
+	public double getCompatabilityMeasure(Characteristic otherRating){
+		int r1 = rating;
+		int r2 = otherRating.getRating();
+		
+		if(r1 == 0 || r2 == 0){
+			return 0.0;
+		}else {
+			return (1.0 - (r1 - r2)*(r1 - r2)/81.0);
+		}
+	}
+		
 	public String getDescription() {
 		return description;
 	}
