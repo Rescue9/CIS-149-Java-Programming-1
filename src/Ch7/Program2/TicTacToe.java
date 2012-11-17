7bc82e96b3bafa5433b825ea064ff38ee6c1f088
Rescue9 <Rescue9@yahoo.com>
2012-11-16 20:11:53 -0600

    altered Ch7Program1. logic is working... now just time to pretty up and fix while for O & X

--- src/Ch7/Program2/TicTacToe.java
+++ src/Ch7/Program2/TicTacToe.java
@@ -19,6 +19,10 @@ public class TicTacToe {
 		board = new char[3][3];
 	}
 	
+	//create new object TicTacToe
+	private static TicTacToe myGame = new TicTacToe();
+
+	
 	// draw the board initially & redraw the board as needed for guess
 	public String drawBoard(){
 		String boardString = "\n\n    Row/Col\n\n";
@@ -45,106 +49,127 @@ public class TicTacToe {
 		}
 	}
 
-	private boolean checkWin(int case){ // CHECK VALIDITY OF THIS SWITCH STATEMENT
+	private boolean checkHoriz(){
 		boolean win = true;
-		
-		switch (nextCase) {
-			case 1: win = win && checkHoriz();
-					return win;
-					break;
-					
-			case 2: win = win && checkVert();
-					return win;
-					break;
-					
-			case 3: win = win && checkDiagL2R();
-					return win;
-					break;
-					
-			case 4: win = win && checkDiagR2L();
-					return win;
-					break;
+		int row = 0;
+		for (row=0; row<=2; row++){
+			System.out.println(row + " row check");
+			if (board[row][0] != board[row][1]){
+				win = false;
+			}else if (board[row][0] != board[row][2]){
+				win = false;
+			}
 		}
-		return null;
-	}
-	
-	private boolean checkHoriz(int row){
-		boolean win = true;
-		int i=0;
-		if (board[row][i] != board[row][i+1]){
-			win = false;
-			}
-		if (board[row][i] != board[row][i+2]){
-			win = false;
-			}
 		return win;
-		}
+	}
 
-	private boolean checkVert(int col){
+	private boolean checkVert(){
 		boolean win = true;
-		int i=0;
-		if (board[i][col] != board[i+1][col]){
-			win = false;
+		int col=0;
+		for (col=0; col<=2; col++){
+			System.out.println(col + " col check");
+			if (board[0][col] != board[1][col]){
+				win = false;
+			}else if (board[1][col] != board[2][col]){
+				win = false;
 			}
-		if (board[i][col] != board[i+2][col]){
-			win = false;
-			}
+		}
 		return win;
 		}
 		
 	private boolean checkDiagL2R(){
 		boolean win = true;
-		if (board[0][0] != board[1][1]{
+		if (board[0][0] != board[1][1] || board[1][1] != board[2][2]){
 			win = false;
-		}
-		if (board[0][0] != board[2][2]{
+		} else if (board[1][1] != 'O' && board[1][1] != 'X'){
 			win = false;
 		}
 		return win;
+	}
+	
+	private boolean checkDiagR2L(){
+		boolean win = true;
+		if (board[0][2] != board[1][1] || board[1][1] != board[2][0]){
+			win = false;
+		}else if (board[1][1] != 'O' && board[1][1] != 'X'){
+			win = false;
+		}
+		return win;
+	}
+	
+	private static void weHaveAWinner(){
+		System.out.println("YOU WIN!"); //TODO determine who actually won
+		System.exit(0);
 	}
 
 	
 	public static void main(String[] args){
 		
-		TicTacToe myGame = new TicTacToe();
 		
-		Scanner keyboard = new Scanner(System.in);
 		System.out.println("\tTicTacToe Game:");
 		System.out.println();
 		System.out.println("The puzzle is:" + myGame.drawBoard());
 		boolean win = false;
-		
-		while(!win){
-			System.out.println("X's turn");
-			System.out.println("Where do you want X placed?");
-			System.out.println("Please enter row number and column number seperated by a space");
-			int row = keyboard.nextInt();
-			int col = keyboard.nextInt();
-			char player = 'X';
-			myGame.addChar(row, col, player);
-			System.out.println(board[row-1][col-1]); // TODO Stub
-			System.out.println(player);
-			System.out.println("the puzzle now is: \n" + myGame.drawBoard());
-			
-			for (int i = 0; i < 2; i++){
-				while(!boolean myGame.checkWin){
-					myGame.checkWin(i);
-					didIWin = myGame.checkWin();
-					System.out.println(didIWin);
-			};
-			System.out.println(didIWin);
-			
-			//win = true; // TODO Stub
-			
-		/*	while(!win){
-				System.out.println("O's turn");
-				System.out.println("Where do you want O placed?");
-				System.out.println("Please enter row number and column number seperated by a space");
-
-				myGame.checkWin();
-				win = true; // TODO Stub
+		boolean xsTurn = true;
+		while (!win){
+			if (xsTurn){
+				takeMyTurn('X');
+				//xsTurn = false;
+			}
+			/*if (!xsTurn){
+				takeMyTurn('O');
+				xsTurn = true;
 			}*/
 		}
 	}
+		
 
+	private static void takeMyTurn(char who){
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println(who + "'s turn");
+		System.out.println("Where do you want " + who +" placed?");
+		System.out.println("Please enter row number and column number seperated by a space");
+		int row = keyboard.nextInt();
+		int col = keyboard.nextInt();
+		char player = who;
+		
+		String boardPosition = board[row-1][col-1] + " is placed here.";
+		System.out.println(boardPosition);
+		
+		// check to see if the space is empty
+		if (board[row-1][col-1] != 'X'){
+			if (board[row-1][col-1] != 'O'){
+				myGame.addChar(row, col, player);
+			}
+		}
+		else {
+			System.out.println("The spot " + row + " " + col + " is not empty.");
+			System.out.println("Try a different space....");
+			System.out.println(myGame.drawBoard());
+			takeMyTurn(player);
+		}
+		
+		System.out.println("the puzzle now is: \n" + myGame.drawBoard());
+		
+		boolean didIWin = myGame.checkVert();
+		System.out.println(" Well.... did " + who + " win: " + didIWin); // TODO Stub
+		if (didIWin){
+			weHaveAWinner();
+		}
+		didIWin = myGame.checkHoriz();
+		System.out.println(" Well.... did " + who + " win: " + didIWin); // TODO Stub
+		if (didIWin){
+			weHaveAWinner();
+		}
+		didIWin = myGame.checkDiagL2R();
+		System.out.println(" Well.... did " + who + " win: " + didIWin); // TODO Stub
+		if (didIWin){
+			weHaveAWinner();
+		}
+		didIWin = myGame.checkDiagR2L();
+		System.out.println(" Well.... did " + who + " win: " + didIWin); // TODO Stub
+		if (didIWin){
+			weHaveAWinner();
+		}
+	}
 }
